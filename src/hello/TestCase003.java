2927c9da5dd492acbe4239c452f55f7e459998dc
kml <fatih200530@hotmail.com>
2022-04-22 00:05:53 +0300

    TestCase003 creating conflict in master

---
+++ src/hello/TestCase003.java
@@ -0,0 +1,9 @@
+package hello;
+
+public class TestCase003 {
+
+    //This is unknown changes to feature branch
+    // that exist in master
+    // now git will not know what to do with this
+    // when we merge and conflict will happen
+}
